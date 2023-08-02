200e3bce7adf8bc7223bb281679453fcfa3500c0
vadim-ma <vadimmitin@mail.ru>
2023-08-02 03:24:05 +0300

    feat: implement all tests

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+    return Number(reverseStr(Math.abs(n).toString()));;
+}
+function reverseStr(str){
+    let result = '';
+    for(let i = 0; i < str.length; i++){
+        result = `${str[i]}${result}`;
+    }
+    return result;
 }
